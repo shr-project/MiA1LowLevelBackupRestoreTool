55a01eb364b48616783551ca3459b104a17b55aa
Martin Jansa <Martin.Jansa@gmail.com>
2019-03-01 12:43:30 +0100

    backup-in-recovery.sh: restore-in-recovery.sh: add support for md5sums
    * with faulty usb3 hub I had some files transfered only partially to the device
      with adb push and then restoring them lead to unusable image, it also allows
      to skip writting the partitions which are identical in backup and the device
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- bin/backup-in-recovery.sh
+++ bin/backup-in-recovery.sh
@@ -7,7 +7,23 @@ fi
 
 if [ ! -d $1 ] ; then
     echo "First parameter '$1' isn't a directory"
-    exit 2
+    echo "Enter 'Y' to create it or anything else to exit"
+    read A
+    if [ "${A}" != "Y" ] ; then
+        exit 2
+    fi
+    mkdir -p $1
+else
+    if ls $1/* 2>/dev/null >/dev/null; then
+        echo "$1 already exists and it's not empty:"
+        ls $1/*
+        echo "Enter 'Y' to empty it or anything else to exit"
+        read A
+        if [ "${A}" != "Y" ] ; then
+            exit 2
+        fi
+        rm -rf $1/*
+    fi
 fi
 if [ ! -f $2 ] ; then
     echo "Second parameter '$2' isn't a file with list of partitions to restore"
@@ -26,6 +42,12 @@ for P in ${PARTITIONS}; do
     [ -e /dev/block/bootdevice/by-name/${P} ] || echo "ERR: partition ${P} doesn't exist on bootdevice /dev/block/bootdevice/by-name/${P}"
 done
 
+echo "Use gzip to create backups?, Enter 'Y' or anything else to skip compression"
+read COMP
+
+echo "Create md5sums?, Enter 'Y' or anything else to skip md5sums"
+read SUMS
+
 echo "Enter 'Y' to continue or anything else to exit"
 read A
 
@@ -34,12 +56,48 @@ if [ "${A}" != "Y" ] ; then
     exit 0
 fi
 
+cp $2 ${DIR}/partition_list.txt
+
 for P in ${PARTITIONS}; do
     if [ -e /dev/block/bootdevice/by-name/${P} ] ; then
-        dd if=/dev/block/bootdevice/by-name/${P} of=${DIR}/${P}.img
+        DEV=`readlink /dev/block/bootdevice/by-name/${P}`
+        PART=`basename ${DEV}`
+        SIZE=`cat /sys/block/mmcblk0/${PART}/size`
+        # from 512b blocks to kb
+        SIZE=`expr ${SIZE} / 2`
+        if [ "${SUMS}" = "Y" ] ; then
+            echo "md5sum ${P}"
+            md5sum /dev/block/bootdevice/by-name/${P} | tee ${DIR}/${P}.part.md5
+        fi
+        echo "Starting to backup partition ${P}, ${PART}, size ${SIZE}kb"
+        echo "${SIZE}" > ${DIR}/${P}.part.size
+        if [ "${COMP}" != "Y" ] ; then
+            OUT=${DIR}/${P}.img
+            dd if=/dev/block/bootdevice/by-name/${P} of=${OUT}
+        else
+            OUT=${DIR}/${P}.img.gz
+            dd if=/dev/block/bootdevice/by-name/${P} | gzip > ${OUT}
+        fi
+        if [ "${SUMS}" = "Y" ] ; then
+            echo "md5sum ${OUT}"
+            md5sum ${OUT} | sed "s#${DIR}/##g" > ${OUT}.md5
+            if [ "${COMP}" != "Y" ] ; then
+                FILESIZE=`du -k ${OUT} | awk '{print $1}'`
+                PARTSIZE=`cat ${DIR}/${P}.part.size`
+                [ "${FILESIZE}" != "${PARTSIZE}" ] && echo "ERR: backup file size ${FILESIZE}kb doesn't match with partition size ${PARTSIZE}kb"
+                FILESUM=`cat ${OUT}.md5 | awk '{print $1}'`
+                PARTSUM=`cat ${DIR}/${P}.part.md5 | awk '{print $1}'`
+                [ "${FILESUM}" != "${PARTSUM}" ] && echo "ERR: backup md5sum ${FILESUM} doesn't match with partition md5sum ${PARTSUM}"
+            fi
+        fi
     else
         echo "ERR: partition ${P} doesn't exist on bootdevice /dev/block/bootdevice/by-name/${P}"
     fi
 done
+if [ "${SUMS}" = "Y" ] ; then
+    cat ${DIR}/*.img*.md5 > ${DIR}/md5sums
+    cat ${DIR}/*.part.md5 > ${DIR}/md5sums.part
+fi
 
+echo "Calling sync to make sure all files are written"
 sync
--- bin/restore-in-recovery.sh
+++ bin/restore-in-recovery.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 
-if [ $# -ne 2 ] ; then
-    echo "Usage $0 directory-to-restore file-with-list-of-partitions-to-restore"
+if [ $# -ne 2 -a $# -ne 1 ] ; then
+    echo "Usage $0 directory-to-restore [file-with-list-of-partitions-to-restore]"
     exit 1
 fi
 
@@ -9,13 +9,22 @@ if [ ! -d $1 ] ; then
     echo "First parameter '$1' isn't a directory"
     exit 2
 fi
-if [ ! -f $2 ] ; then
-    echo "Second parameter '$2' isn't a file with list of partitions to restore"
-    exit 3
+if [ $# -eq 1 ] ; then
+    LIST=$1/partition_list.txt
+    if [ ! -f "${LIST}" ] ; then
+        echo "Default list $1/partition_list.txt doesn't exist, you need to provide 2nd parameter with the list of partitions"
+        exit 3
+    fi
+else
+    LIST=$2
+    if [ ! -f "${LIST}" ] ; then
+        echo "Second parameter '${LIST}' isn't a file with list of partitions to restore"
+        exit 4
+    fi
 fi
 
 DIR=`echo $1 | sed 's#/$##'`
-PARTITIONS=`cat $2 | grep -v "^#" | sed 's/\r/\n/g' | xargs`
+PARTITIONS=`cat ${LIST} | grep -v "^#" | sed 's/\r/\n/g' | xargs`
 
 echo "Restore following partitions: ${PARTITIONS} from directory ${DIR}?"
 
@@ -23,11 +32,14 @@ for P in ${PARTITIONS}; do
     [ -e /dev/block/bootdevice/by-name/${P} ] || echo "ERR: partition ${P} doesn't exist on bootdevice /dev/block/bootdevice/by-name/${P}"
 done
 for P in ${PARTITIONS}; do
-    [ -f ${DIR}/${P}.img ] || echo "ERR: ${DIR}/${P}.img doesn't exist for requested partition ${P}"
+    [ -f ${DIR}/${P}.img -o -f ${DIR}/${P}.img.gz ] || echo "ERR: ${DIR}/${P}.img(.gz) doesn't exist for requested partition ${P}"
 done
-for P in `ls ${DIR}/*img | sed 's#.*/##g; s#\.img##g'`; do
-    cat $2 | grep -v "^#" | sed 's/\r/\n/g' | grep -q "^${P}$" || echo "WARN: Skipping ${DIR}/${P}.img"
+for P in `ls ${DIR}/*img ${DIR}/*img.gz 2>/dev/null | sed 's#.*/##g; s#\.gz##g; s#\.img##g'`; do
+    cat ${LIST} | grep -v "^#" | sed 's/\r/\n/g' | grep -q "^${P}$" || echo "WARN: Skipping ${DIR}/${P}.img"
 done
+
+echo "Check checksums after restore? And skip identical partitions? Enter 'Y' to continue or anything else to exit"
+read SUMS
 
 echo "Enter 'Y' to continue or anything else to exit"
 read A
@@ -38,13 +50,49 @@ if [ "${A}" != "Y" ] ; then
 fi
 
 for P in ${PARTITIONS}; do
-    if [ -f ${DIR}/${P}.img -a -e /dev/block/bootdevice/by-name/${P} ] ; then
-        dd if=${DIR}/${P}.img of=/dev/block/bootdevice/by-name/${P}
-    elif [ ! -f ${DIR}/${P}.img ] ; then
+    if [ ! -f ${DIR}/${P}.img -a ! -f ${DIR}/${P}.img.gz ] ; then
+        echo "ERR: ${DIR}/${P}.img(.gz) doesn't exist for requested partition ${P}"
+    elif [ -e /dev/block/bootdevice/by-name/${P} ] ; then
+        DEV=`readlink /dev/block/bootdevice/by-name/${P}`
+        PART=`basename ${DEV}`
+        SIZE=`cat /sys/block/mmcblk0/${PART}/size`
+        # from 512b blocks to kb
+        SIZE=`expr ${SIZE} / 2`
+        IN=${DIR}/${P}.img
+        if [ ! -f ${IN} ] ; then
+            IN=${IN}.gz
+            COMP=Y
+        fi
+        if [ "${COMP}" != "Y" ] ; then
+            FILESIZE=`du -k ${IN} | awk '{print $1}'`
+            PARTSIZE=`cat ${DIR}/${P}.part.size`
+            [ "${FILESIZE}" != "${PARTSIZE}" ] && echo "ERR: ${P} backup file size ${FILESIZE}kb doesn't match with stored partition size ${PARTSIZE}kb, press ENTER to continue" && read
+            [ "${SIZE}" != "${PARTSIZE}" ] && echo "ERR: current ${P} partition size ${SIZE}kb doesn't match with stored partition size ${PARTSIZE}kb, press ENTER to continue" && read
+            FILESUM=`md5sum ${IN} | awk '{print $1}'`
+            PARTSUM=`cat ${IN}.md5 | awk '{print $1}'`
+            [ "${FILESUM}" != "${PARTSUM}" ] && echo "ERR: ${P} backup md5sum ${FILESUM} doesn't match with stored md5sum ${PARTSUM}, press ENTER to continue" && read
+        fi
+        if [ "${SUMS}" = "Y" ] ; then
+            PARTSUM=`md5sum /dev/block/bootdevice/by-name/${P} | awk '{print $1}'`
+            SAVEDSUM=`cat ${DIR}/${P}.part.md5 | awk '{print $1}'`
+            [ "${FILESUM}" = "${PARTSUM}" ] && echo "md5 for partition ${P} is identical in backup and the device, skipping this partition" && continue
+        fi
+        echo "Starting to restore partition ${P}, ${PART}, size ${SIZE}kb"
+        if [ "${COMP}" != "Y" ] ; then
+            dd if=${IN} of=/dev/block/bootdevice/by-name/${P}
+        else
+            gzip -cd ${IN} | dd of=/dev/block/bootdevice/by-name/${P}
+        fi
+    elif [ ! -f ${DIR}/${P}.img -a ! -f ${DIR}/${P}.img.gz ] ; then
         echo "ERR: ${DIR}/${P}.img doesn't exist for requested partition ${P}"
     else
         echo "ERR: partition ${P} doesn't exist on bootdevice /dev/block/bootdevice/by-name/${P}"
     fi
 done
+if [ "${SUMS}" = "Y" -a -f "${DIR}/md5sums.part" ] ; then
+    echo "Testing the md5sums from ${DIR}/md5sums.part"
+    md5sum -c ${DIR}/md5sums.part
+fi
 
+echo "Calling sync to make sure all files are written"
 sync
